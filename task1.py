f5ee1751b8e4cd11acb8115e089913e3b92ba1f8
Diya Dobariya <diyadobariya195@gmail.com>
2025-08-24 21:24:40 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("enter a no"))
+b=int(input("enter a no"))
+addition=a+b
+print("Addtion " ,addition)
+subtraction=a-b
+print("subtraction ",subtraction)
+multiplication=a*b
+print("Multipilcation ",multiplication)
+division=a/b
+print("division ",division)
